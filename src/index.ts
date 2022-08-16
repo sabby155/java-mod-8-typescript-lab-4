ee4d700e1eb4ad6f83c545c959c467152d749efb
sabby155 <nebula155@gmail.com>
2022-08-16 12:21:08 -0400

    ts lab 4 complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 // your code here
+
+const button = document.getElementById("btn") as HTMLButtonElement;
+const input1 = document.getElementById("firstInput") as HTMLInputElement;
+const input2 = document.getElementById("secondInput") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+    console.log(prepend([input1!.value, input2!.value]));
+});
+
+function prepend<T>(array: T[]) {
+    // your code here
+    let space: string = "";
+    let numOfSpaces = +array[1] as number;
+    let word = array[0]
+
+    for (let i = 0; i < numOfSpaces; i++) {
+        space += " ";
+    }
+
+    console.log("number of spaces:", numOfSpaces);
+    console.log("amount of space: ", "|" + space +"|");
+    
+    return `${space}${word}`;
+}
+
+
+
